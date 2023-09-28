8a236e0dc38767721e11209078022d95edad9ff1
Pedro Pessoa <pedropsb95@gmail.com>
2023-09-28 19:22:43 -0300

    pds(2): add eh_par(int) -> bool

--- pds-02-functions/numeros-pares.c
+++ pds-02-functions/numeros-pares.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+int eh_par(int numero) {
+  if (numero % 2 == 0) {
+    return 1;
+  }
+  return 0;
+}
+
 int main() {
   for (int i = 0; i <= 1000; i++) {
-    if (i % 2 == 0) {
+    if (eh_par(i)) {
       printf("%d ", i);
     }
   }
